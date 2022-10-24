cbd9fde9bcd7adc2a06a97297b28527997a0bf1c
zhangqiang <tyokyo320@gmail.com>
2022-10-25 02:18:57 +0900

    🎈 perf: update

--- utils/data_processing_electricity.py
+++ utils/data_processing_electricity.py
@@ -24,7 +24,7 @@ class ElectricityDataset(torch.utils.data.Dataset):
         return self.X[key], self.Y[key], self.sequence_lengths[key]
 
 
-def get_raw_electricity_data(cached=True):
+def get_raw_electricity_data(cached=True) -> np.ndarray:
     if cached:
         with open("data/electricity.pkl", "rb") as f:
             dataset = pickle.load(f)
@@ -33,23 +33,40 @@ def get_raw_electricity_data(cached=True):
         df = pd.read_csv("data/electricity.csv")
         print(df)
 
+    return dataset
 
-def get_electricity_splits():
+
+def get_electricity_splits(length=100, horizon=50, conformal=True, n_train=200, n_calibration=100, n_test=80, cached=True, seed=None) -> ElectricityDataset:
     pass
 
 
-def convert_daliy_electricity_data() -> None:
+def convert_daily_electricity_data() -> None:
     df = pd.read_csv('data/LoadData.csv')
     # Aggregating the dataset at daily level
     df['Timestamp'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S')
     df.index = df['Timestamp']
     df = df.resample('D').mean()
     print(df)
-    df.to_csv('data/electricity.csv')
+    df.to_csv('data/electricity_daily.csv')
 
+
+def convert_monthly_electricity_data() -> None:
+    df = pd.read_csv('data/LoadData.csv')
+    # Aggregating the dataset at monthly level
+    df['Time'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S')
+    df.index = df['Time']
+    df = df.resample('M').mean()
+    # generate "year-month" format column
+    df['Timestamp'] = df.index.strftime('%Y-%m')
+    df = df.reset_index(drop=True)
+    df.index = df['Timestamp']
+    del df['Timestamp']
+    print(df)
+    df.to_csv('data/electricity_monthly.csv')
 
 if __name__ == '__main__':
-    convert_daliy_electricity_data()
+    # convert_daily_electricity_data()
+    convert_monthly_electricity_data()
 
 
 
